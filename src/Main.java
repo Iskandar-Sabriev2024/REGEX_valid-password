9a8e4ccce61866c2bf4d26ca5c22a8416e764a6a
Iskandar-Sabriev <sabriev.i90@gmail.com>
2024-12-18 09:43:50 +0500

    homework commit 1

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        // Test parollar
+        String[] testPasswords = {
+                "Abc123!@#",
+                "password",
+                "PASSWORD123",
+                "Pass123",
+                "Pass123#",
+                "12345678"
+        };
+
+        for (String password : testPasswords) {
+            if (isValidPassword(password)) {
+                System.out.println(password + " - Parol to'g'ri");
+            } else {
+                System.out.println(password + " - Parol noto'g'ri");
+            }
+        }
+    }
+
+    public static boolean isValidPassword(String password) {
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,}$";
+        return Pattern.matches(regex, password);
+    }
+}
+
